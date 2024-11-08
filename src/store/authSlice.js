79f6485b7f1c9d2dc752ed1f3ee6378b3b6400ce
arif raza <arifraza46815@gmail.com>
2024-11-08 11:46:01 +0530

    store configuration is added to track user login status.

---
+++ src/store/authSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    isLoggedIn : false,
+    userData : null
+}
+
+const authSlice = createSlice({
+    name : "auth",
+    initialState,
+    reducers : {
+        login : (state , action) => {
+            state.isLoggedIn = true;
+            state.userData = action.payload
+        },
+        logout : (state) => {
+            state.isLoggedIn = false;
+        }
+    }
+})
+
+export const { login, logout} = authSlice.actions
+export default authSlice.reducer;
